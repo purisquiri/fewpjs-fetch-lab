9879e4d3b363c582586cd485e18d780caa6ce130
purisquiri <mau.dorigo@outlook.es>
2020-11-19 09:25:01 -0500

    commit tree 87e50dd1678ec41240bc258a6e4893a12f0716cc

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
+  return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => console.log(renderBooks(json))); 
+  .then(json => renderBooks(json)); 
+ 
 }
 
 function renderBooks(books) {
